ac9bbffe1f331952140cd373611b66ec6994d056
iridescent <2389504513@qq.com>
2024-05-24 01:12:24 +0800

    ✨ feat: 测试音视频转换

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import { fetchFile, toBlobURL } from '@ffmpeg/util';
 import { useEffect, useRef, useState } from 'react';
 
 function App() {
-  const ffmpeg = useRef(new FFmpeg()).current;
+  const ffmpeg = useRef(new FFmpeg()).current
 
-  const [loaded, setLoaded] = useState(false)
+  const [video, setVideo] = useState<string>()
+
+  const [audio, setAudio] = useState<string>()
 
   const load = async () => {
     const baseURL = "https://unpkg.com/@ffmpeg/core-mt@0.12.6/dist/esm";
@@ -18,34 +20,62 @@ function App() {
       coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
       wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
       workerURL: await toBlobURL(`${baseURL}/ffmpeg-core.worker.js`, 'text/javascript')
-    })
-
-    setLoaded(true)
+    });
   }
-  const convert = async () => {
-    try {
-      await ffmpeg.writeFile('audio.amr', await fetchFile('./assets/audio.amr'))
-      await ffmpeg.exec(['-i', 'audio.amr', 'audio.mp3'])
-      await ffmpeg.readFile('input.mp3')
-    } catch (error) {
-      console.log(error)
-    }
+
+  const convertWav2Mp3 = async (download: boolean = false) => {
+    const audioURL = "https://raw.githubusercontent.com/ffmpegwasm/testdata/master/audio-15s.wav";
+    await ffmpeg.writeFile('audio.wav', await fetchFile(audioURL));
+    await ffmpeg.exec(['-i', 'audio.wav', 'audio.mp3']);
+    const fileData = await ffmpeg.readFile('audio.mp3');
+    const data = new Uint8Array(fileData as ArrayBuffer);
+    download && downLoad(data, 'mp3')
+    setAudio(URL.createObjectURL(new Blob([data], { type: 'audio/mp3' })));
+  }
+
+  const downLoad = async (data: BlobPart, fileType: string) => {
+    const anchor = document.createElement('a')
+    anchor.href = URL.createObjectURL(new Blob([data], { type: `media/${fileType}` }));
+    anchor.download = `media.${fileType}`;
+    anchor.click();
+  }
+
+  const convertAvi2Mp4 = async () => {
+    const videoURL = "https://raw.githubusercontent.com/ffmpegwasm/testdata/master/video-15s.avi";
+    await ffmpeg.writeFile('video.avi', await fetchFile(videoURL));
+    await ffmpeg.exec(['-i', 'video.avi', 'video.mp4']);
+    const fileData = await ffmpeg.readFile('video.mp4');
+    const data = new Uint8Array(fileData as ArrayBuffer);
+    setVideo(URL.createObjectURL(new Blob([data], { type: 'video/mp4' })));
   }
 
   const start = async () => {
-    await load()
-    await convert()
+    try {
+      await load();
+      await Promise.all([
+        convertWav2Mp3(),
+        convertAvi2Mp4(),
+      ])
+    } catch (error) {
+      console.log(error);
+    }
+
   }
 
   useEffect(() => {
-    start()
-  }, [])
+    start();
+
+    return () => {
+      ffmpeg.off("log", () => { }); // 取消日志事件监听
+    };
+  }, []);
 
   return (
     <div>
-      {String(loaded)}
+      <video src={video} controls></video>
+      <audio src={audio} controls></audio>
     </div>
   )
 }
 
-export default App
+export default App;
